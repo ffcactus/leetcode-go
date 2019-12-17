11d0e10eb4647560cd3b48cf38b3b224b82eea1a
b00392874 <baibin3@huawei.com>
2019-12-17 15:09:00 +0800

    Add cases that use graph.

---
+++ graph/cycle/cycle.go
@@ -0,0 +1,47 @@
+// Package cycle provide an implementation to check if there is a cycle in the graph.
+// The implementation use DFS to solve the problem.
+package cycle
+
+import "leetcode-go/graph"
+
+// DFSImpl use DFS to check if there is a cycle in the graph.
+type DFSImpl struct {
+	marked []bool
+	hasCycle bool
+}
+
+// NewDFSImpl generate the DFSImpl from a graph.
+func NewDFSImpl(g graph.Graph) *DFSImpl {
+	impl := DFSImpl{
+		marked:   make([]bool, g.Vertices()),
+		hasCycle: false,
+	}
+	for i := 0; i < g.Vertices(); i++ {
+		if !impl.marked[i] {
+			impl.dfs(g, i, i)
+		}
+	}
+	return &impl
+}
+
+func (impl *DFSImpl) dfs(g graph.Graph, next, previous int) {
+	if impl.hasCycle {
+		return
+	}
+	impl.marked[next] = true
+	for _, w := range g.Adjacent(next) {
+		if !impl.marked[w] {
+			impl.dfs(g, w, next)
+		} else if w != previous { // if your next's next vertex is marked and not equals to your previous vertex, it means there is a cycle.
+			impl.hasCycle = true
+			return
+		}
+	}
+}
+
+// HasCycle return if there is a cycle in the graph.
+func (impl *DFSImpl) HasCycle() bool {
+	return impl.hasCycle
+}
+
+
---
+++ graph/twocolor/twocolor.go
@@ -0,0 +1,42 @@
+// Package twocolor provide the solution to check if the graph's vertex can be colored in two color that no edge connected to
+// the vertices with the same color.
+package twocolor
+
+import "leetcode-go/graph"
+
+// DFSImpl provider the DFS solution to solve the two color problem.
+type DFSImpl struct {
+	marked []bool
+	color []bool
+	isTwoColorable bool
+}
+
+// NewDFSImpl generate the DFSImpl from graph.
+func NewDFSImpl(g graph.Graph) *DFSImpl {
+	impl := DFSImpl{
+		marked:         make([]bool, g.Vertices()),
+		color:          make([]bool, g.Vertices()),
+		isTwoColorable: true,
+	}
+	return &impl
+}
+
+func (impl *DFSImpl) dfs(g graph.Graph, v int) {
+	if impl.isTwoColorable == false {
+		return
+	}
+	impl.marked[v] = true
+	for _, w := range g.Adjacent(v) {
+		if !impl.marked[w] {
+			impl.color[w] = !impl.color[v]
+			impl.dfs(g, w);
+		} else if impl.color[w] == impl.color[v] {
+			impl.isTwoColorable = false
+			return
+		}
+	}
+}
+
+func (impl *DFSImpl) IsTwoColorable() bool {
+	return impl.isTwoColorable
+}
